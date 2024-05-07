bf1aec574a45f13982cc84014deeccfd1fd4f885
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-07 15:35:42 -0400

    complete logo component setup

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,12 @@
 import Tilt from "react-parallax-tilt";
 import "./Logo.css";
+import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return(
     <div className="ma4 mt0">
-      <Tilt className="br2 shadow-2 tilt">
-        <div>
-          <h1>React Parallax Tilt 👀</h1>
-        </div>
+      <Tilt className="br2 shadow-2 tilt pointer pa3">
+        <img src={logo} alt="transparent brain logo" />
       </Tilt>
     </div>
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     <nav className="nav">
       <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
     </nav>
-  )
-}
+  );
+};
 
 export default Navigation;
