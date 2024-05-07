c19a7ce3929d6acddde8ad09d88acf77d7293bc5
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-07 15:09:21 -0400

    set up Navigation & Logo component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* {<Logo />
+      <Logo />
+      {/*
       <ImageLinkForm />
       <FaceRecognition />} */}
     </div>
