6f8ab0c928b8b1b6927d7c548065d50b33f15a41
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-07 20:22:57 -0400

    style image link form

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -7,7 +7,7 @@ const ImageLinkForm = () => {
       <p className="f3 pa3">
         {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
-      <div className="pa4 br3 shadow-5 image-form-input">
+      <div className="pa3 br3 shadow-5 image-form-input">
         <input className="f4 pa2 w-70 center" type="text" />
         <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer">Detect</button>
       </div>
