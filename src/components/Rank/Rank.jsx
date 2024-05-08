4f3b038299545c4193b62c374dcbde88fae5d31c
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-07 20:50:15 -0400

    commit all changes

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+const Rank = () => {
+  return(
+    <div className="center">
+      <div className="white f3">
+        {'Daryl, Your current rank is ...'}
+      </div>
+      <div className="white f1">
+        {'#5'}
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
