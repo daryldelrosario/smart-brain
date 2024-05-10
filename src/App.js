de5a715a2e1ea07bb0413600341019a98519eca0
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-10 15:50:12 -0400

    connect ImageLinkForm to App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
@@ -7,13 +8,26 @@ import './App.css';
 
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const onInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  const onButtonSubmit = () => {
+    console.log('You Clicked the Detect Button');
+  }
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" color="#F5F5F5" num={33} bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/*
       <FaceRecognition />} */}
     </div>
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,6 +1,6 @@
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 
   return(
     <div className="image-form-container">
@@ -8,8 +8,17 @@ const ImageLinkForm = () => {
         {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
       <div className="pa3 br3 shadow-5 image-form-input">
-        <input className="f4 pa2 w-70 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer">Detect</button>
+        <input 
+          className="f4 pa2 w-70 center" 
+          type="text" 
+          onChange={onInputChange}
+        />
+        <button 
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer"
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
       </div>
     </div>
 
