d2e76ecca23eb3a625eff79802a7b61529d35055
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-10 17:30:38 -0400

    style the user input image

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,7 +1,12 @@
+import "./FaceRecognition.css";
+
 const FaceRecognition = ({ imageURL }) => {
   return(
-    <div className="center">
-      <img src={imageURL} alt="user input image" />
+    <div className="center ma">
+      <div className="img-container">
+        <img src={imageURL} alt="user input image" className="img" /> 
+      </div>
+      
     </div>
   )
 }
