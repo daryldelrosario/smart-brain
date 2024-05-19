e3f0e1cf33c63854f77041138f24aea09e4ee3bb
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-19 10:48:15 -0400

    start signin functionality

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./SignIn.css";
 
 const SignIn = ({ setIsSignedIn }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
 
   const handleSignIn = (e) => {
     e.preventDefault();
@@ -11,20 +15,39 @@ const SignIn = ({ setIsSignedIn }) => {
     setIsSignedIn("register");
   }
 
+  const onSignInSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    setIsSignedIn("home");
+  }
+
   return (
     <>
       <article className="br3 ba b--black-10 mv4 w-100 w-75-m w-50-l mw6 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure" onSubmit={handleSignIn}>
+          <form className="measure" onSubmit={onSignInSubmit}>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" for="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                  type="email" 
+                  name="email-address"  
+                  id="email-address" 
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" for="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password" 
+                  name="password"  
+                  id="password" 
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
             </fieldset>
             <div className="">
