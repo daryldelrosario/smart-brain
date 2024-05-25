da0424a444a782360d979ca8ed8e08b1965a9b89
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-05-25 13:38:17 -0400

    attempt to bring authentication to the backend server

--- src/App.js
+++ src/App.js
@@ -91,42 +91,15 @@ function App() {
   useEffect(() => {
     const fetchImageRegions = () => {
       if(imageURL.trim() !== "") {
-        const PAT = '4cefee143f5545ed861ae3abf80dff2b';
-        const USER_ID = 'clarifai';
-        const APP_ID = 'main';
-        const MODEL_ID = 'face-detection';
-        const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-  
-        const raw = JSON.stringify({
-          "user_app_id": {
-              "user_id": USER_ID,
-              "app_id": APP_ID
-          },
-          "inputs": [
-              {
-                  "data": {
-                      "image": {
-                          "url": imageURL
-                      }
-                  }
-              }
-          ]
-        });
-  
-        const requestOptions = {
+        fetch("http://localhost:3001/clarifai", {
           method: 'POST',
           headers: {
-              'Accept': 'application/json',
-              'Authorization': 'Key ' + PAT
+            'Content-Type': 'application/json'
           },
-          body: raw
-        };
-  
-        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+          body: JSON.stringify({ imageURL: imageURL })
+        })
           .then(response => response.json())
-          .then(result => {
-            const regions = result.outputs[0].data.regions;
-
+          .then(regions => {
             if(regions) {
               calcFaceLocation(regions);
             } else {
